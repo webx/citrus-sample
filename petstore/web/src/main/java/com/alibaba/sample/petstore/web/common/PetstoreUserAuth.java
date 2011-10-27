f33af552c2604a320335b851028dfc7073c4925a
Michael Zhou <yizhi@taobao.com>
2011-10-26 21:00:46 -0600

    改进petstore：当读取session出错时仍可继续执行下去。

--- petstore/web/src/main/java/com/alibaba/sample/petstore/web/common/PetstoreUserAuth.java
+++ petstore/web/src/main/java/com/alibaba/sample/petstore/web/common/PetstoreUserAuth.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.citrus.service.uribroker.URIBrokerService;
@@ -17,6 +19,8 @@ import com.alibaba.citrus.turbine.TurbineRunData;
 import com.alibaba.citrus.turbine.pipeline.valve.PageAuthorizationValve.Callback;
 
 public class PetstoreUserAuth extends BeanSupport implements Callback<PetstoreUserAuth.Status> {
+    private static final Logger log = LoggerFactory.getLogger(PetstoreUserAuth.class);
+
     @Autowired
     private URIBrokerService uriBrokerService;
 
@@ -55,7 +59,13 @@ public class PetstoreUserAuth extends BeanSupport implements Callback<PetstoreUs
 
     public Status onStart(TurbineRunData rundata) {
         HttpSession session = rundata.getRequest().getSession();
-        PetstoreUser user = (PetstoreUser) session.getAttribute(sessionKey);
+        PetstoreUser user = null;
+
+        try {
+            user = (PetstoreUser) session.getAttribute(sessionKey);
+        } catch (Exception e) {
+            log.warn("Failed to read PetstoreUser from session: " + sessionKey, e);
+        }
 
         if (user == null) {
             // 创建匿名用户
